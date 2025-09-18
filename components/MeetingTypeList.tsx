fe78190b98fb6e22266be04a157aa2efd0697931
manyamehndiart <fycskundan@gmail.com>
2025-09-19 01:54:11 +0530

    shit things removed

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -79,15 +79,12 @@ const MeetingTypeList = () => {
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      {/* Only students can create instant meetings */}
-      {role === 'student' && (
-        <HomeCard
+      <HomeCard
           img="/icons/add-meeting.svg"
           title="New Meeting"
           description="Start an instant meeting"
           handleClick={() => setMeetingState('isInstantMeeting')}
         />
-      )}
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
@@ -95,16 +92,13 @@ const MeetingTypeList = () => {
         className="bg-blue-1"
         handleClick={() => setMeetingState('isJoiningMeeting')}
       />
-      {/* Only teachers can schedule meetings */}
-      {role === 'teacher' && (
-        <HomeCard
+      <HomeCard
           img="/icons/schedule.svg"
           title="Schedule Meeting"
           description="Plan your meeting"
           className="bg-purple-1"
           handleClick={() => setMeetingState('isScheduleMeeting')}
         />
-      )}
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
--- components/RoleGate.tsx
+++ components/RoleGate.tsx
@@ -1,23 +1,8 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useUser } from "@clerk/nextjs";
-import RoleModal from "@/components/RoleModal";
+import React from "react";
 
 const RoleGate = ({ children }: { children: React.ReactNode }) => {
-  const [role, setRole] = useState<string | null>(null);
-  const { user } = useUser();
-
-  useEffect(() => {
-    const storedRole = typeof window !== "undefined" ? localStorage.getItem("userRole") : null;
-    setRole(storedRole);
-  }, []);
-
-  return (
-    <>
-      {user && !role && <RoleModal onSelect={setRole} />}
-      {children}
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default RoleGate;
